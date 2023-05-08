a599e5a9a50764b49e1d1e5c9093128e12483523
Yogesh Sathe <ysathe991@gmail.com>
2023-05-08 18:11:55 +0530

    banner & feature updated

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import './App.css';
 import ResponsiveAppBar from './Components/Home';
 import { Features } from './Components/Features';
 import Footer from './Components/Footer';
-import image from "./Picsart_23-05-06_11-15-10-653.jpg"
+import { Mainbanner } from './Components/Mainbanner';
+
 
 function App() {
   return (
     <div className="App">
       <ResponsiveAppBar />
-      <img style={{ width: '100%', height: "110vh" }} src={image} alt='poster' />
+      <Mainbanner/>
       <Features />
       <hr />
       <Footer />
