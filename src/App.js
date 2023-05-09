f13525de6773ef62639d44806600eff1998ff335
Yogesh Sathe <ysathe991@gmail.com>
2023-05-09 09:05:03 +0530

    folder structure added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
-import ResponsiveAppBar from './Components/Home';
-import { Features } from './Components/Features';
-import Footer from './Components/Footer';
-import { Mainbanner } from './Components/Mainbanner';
+import ResponsiveAppBar from './Components/Navbar/Home.jsx'
+import { Features } from './Components/Features/Features';
+import Footer from './Components/Footer/Footer';
+import { Mainbanner } from './Components/Banner/Mainbanner';
 
 
 function App() {
